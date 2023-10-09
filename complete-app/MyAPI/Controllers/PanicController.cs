bede6ebdd730ef539d77eb5e0abb6da53e3ba425
Bradley Van Aardt <theminidriver@gmail.com>
2023-10-09 18:46:36 +0200

    added roles!

---
+++ complete-app/MyAPI/Controllers/PanicController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PanicController : ControllerBase
+{
+   
+    [HttpGet]
+    [Authorize(Roles = "teller")]
+    [Authorize]
+    public string Get()
+    {
+        return "We've called the police!";
+    }
+}
