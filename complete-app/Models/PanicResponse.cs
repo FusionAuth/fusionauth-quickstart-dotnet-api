ff61e41588f067a3120c8b4f724b369877753785
Bradley Van Aardt <theminidriver@gmail.com>
2023-10-10 12:26:51 +0200

    updated runner

---
+++ complete-app/Models/PanicResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MyAPI.Models
+{
+	public class PanicResponse
+	{
+		public PanicResponse(string message)
+		{
+			Message = message;
+		}
+
+		public string Message { get; set; }
+	}
+}
+
